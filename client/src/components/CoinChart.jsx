192c07923ec67587647ec182301edbf7879e8e9a
Derrick Cross <decross1@me.com>
2018-01-11 09:10:22 -0800

    Added grid structure to app ui, still trying to get things to line up correctly

--- client/src/components/CoinChart.jsx
+++ client/src/components/CoinChart.jsx
@@ -17,33 +17,38 @@ class CoinChart extends React.Component {
 
   render(){
     return (
-      <div className="chart">
-      <div>
-      <button value="1" onClick={this.onClickCoinName.bind(this)}>Bitcoin</button>
-      </div>
-      <div>
-      <button value="2" onClick={this.onClickCoinName.bind(this)}>Ethereum</button>
-      </div>
-      <div>
-      <button value="3" onClick={this.onClickCoinName.bind(this)}>Ripple</button>
-      </div>
-      <div>
-      <button value="4" onClick={this.onClickCoinName.bind(this)}>Litecoin</button>
-      </div>
-        <div>
-        <button value="1H" onClick={this.onClickTimePeriod.bind(this)}>1H</button><button value="1D" onClick={this.onClickTimePeriod.bind(this)}>1D</button><button value="1W" onClick={this.onClickTimePeriod.bind(this)}>1W</button><button value="1M" onClick={this.onClickTimePeriod.bind(this)}>1M</button><button value="1Y" onClick={this.onClickTimePeriod.bind(this)}>1Y</button><button value="ALL" onClick={this.onClickTimePeriod.bind(this)}>ALL</button>
-        </div>
-        <Line data={this.props.chartData} options={{
-          scales: { xAxes: [{ gridLines: { display:false }}], yAxes: [{ gridLines: { display:false }}]},
-          legend: { display: false }
+      <div className="sixteen column row graphRow">
+        <div className="two wide column"></div>
+        <div className="twelve wide column">
+          <Line data={this.props.chartData} height={38} width={100} options={{
+            scales: { xAxes: [{ gridLines: { display:false }}], yAxes: [{ gridLines: { display:false }}]},
+            legend: { display: false }, 
           }}/>
+        </div>
+        <div className="two wide column"></div>
       </div>
-    )
+    );
   }
 }
 
 export default CoinChart;
 
 
-
+// <div>
+// <div>
+//   <button value="1" onClick={this.onClickCoinName.bind(this)}>Bitcoin</button>
+// </div>
+// <div>
+//   <button value="2" onClick={this.onClickCoinName.bind(this)}>Ethereum</button>
+// </div>
+// <div>
+//   <button value="3" onClick={this.onClickCoinName.bind(this)}>Ripple</button>
+// </div>
+// <div>
+//   <button value="4" onClick={this.onClickCoinName.bind(this)}>Litecoin</button>
+// </div>
+// <div>
+//   <button value="1H" onClick={this.onClickTimePeriod.bind(this)}>1H</button><button value="1D" onClick={this.onClickTimePeriod.bind(this)}>1D</button><button value="1W" onClick={this.onClickTimePeriod.bind(this)}>1W</button><button value="1M" onClick={this.onClickTimePeriod.bind(this)}>1M</button><button value="1Y" onClick={this.onClickTimePeriod.bind(this)}>1Y</button><button value="ALL" onClick={this.onClickTimePeriod.bind(this)}>ALL</button>
+// </div>
+// </div>
 

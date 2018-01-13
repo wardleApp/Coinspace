16fb05d30e2d3a057098c8e7daec41850a504f3e
Jackie Jou <jakeic46@gmail.com>
2018-01-13 14:51:13 -0800

    small fix

--- client/src/components/SmallCurrencyToggle.jsx
+++ client/src/components/SmallCurrencyToggle.jsx
@@ -15,7 +15,11 @@ class SmallCurrentToggle extends React.Component {
     return (
       <Menu pointing secondary>
         <Menu.Menu position='left'>
+<<<<<<< f18a08243da5464ee76ee7aa3373e2936b899f28
           <Menu.Item active={this.props.currentCoin === this.props.coin_id} content={`${this.props.name} $${(+this.props.coin).toLocaleString(undefined, {minimumFractionDigits: 2})}`} onClick={this.onClickHandle.bind(this)} value={this.props.coin_id}/>
+=======
+          <Menu.Item active={this.props.currentCoin === this.props.coin_id} content={`${this.props.name} $${(+this.props.coin).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}`} onClick={this.onClickHandle.bind(this)} value={this.props.coin_id}/>
+>>>>>>> small fix
         </Menu.Menu>
       </Menu>
     );
